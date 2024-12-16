09665adf3013a11c6511d2659d2e5b66848b2beb
Leon-prog-sys <l3onmer1@outlook.com.gr>
2024-12-16 13:03:49 +0200

    Fixed bugs

--- intro.c
+++ intro.c
@@ -9,13 +9,13 @@ int main() {
     scanf("%d", &num1);
     
     printf("Enter second number: ");
-    scanf("%d", &num2)
+    scanf("%d", &num2); // Missing ;
 
     // Calculating summary of numbers
-    sum = num1 * num2;
+    sum = num1 + num2; // Replaced * with +
 
     // Printing the result
-    printf("The sum of %s and %d is %d\n", num1, num2, sum);
+    printf("The sum of %d and %d is %d\n", num1, num2, sum); // Replaced %s with %d
 
-    return 0
+    return 0; // Missing ;
 }
